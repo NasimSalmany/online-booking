2002853fb94aa6d2cae45f3239587757822b3647
Nasim salmany <nasimsalmani1993@gmail.com>
2020-04-03 08:34:34 +0200

    Add Eureka servie registery client

--- src/main/java/com/booking/app/service/ScheduleServiceImpl.java
+++ src/main/java/com/booking/app/service/ScheduleServiceImpl.java
@@ -1,7 +1,22 @@
 package com.booking.app.service;
 
+import com.booking.app.entity.Schedule;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 /**
  * @author Nasim Salmany
  */
+@Service
 public class ScheduleServiceImpl implements ScheduleService {
+    @Override
+    public Mono<Schedule> book(Schedule schedule) {
+        return null;
+    }
+
+    @Override
+    public Flux<Schedule> findAll() {
+        return null;
+    }
 }
